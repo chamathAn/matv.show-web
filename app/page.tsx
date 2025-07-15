06199f4b9763d5dc0360da55ac95f49a080f15ed
Chamath Anjula <chamathanjula2@gmail.com>
2025-07-15 14:01:57 +0530

    feat: home hero setup

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,10 @@
+import HomeHero from "@/components/home/home-hero";
 import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
     <main className="bg-background">
-      <span className="font-roboto text-7xl">MATV.SHOW</span>
-      <Button>test</Button>
+      <HomeHero />
     </main>
   );
 }
