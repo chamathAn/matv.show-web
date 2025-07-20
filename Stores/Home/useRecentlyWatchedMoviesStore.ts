d1a608f9f41abfb046d9466a83668bc4632b1905
Chamath Anjula <chamathanjula2@gmail.com>
2025-07-20 18:39:32 +0530

    fix: add mutliple id dummmy functionality for recently watched movies and tv shows

--- Stores/Home/useRecentlyWatchedMoviesStore.ts
+++ Stores/Home/useRecentlyWatchedMoviesStore.ts
@@ -12,19 +12,30 @@ export const useRecentlyWatchedMoviesStore =
   createStore<RecentlyWatchedMoviesStore>()((set) => ({
     recentlyWatchedMovies: [],
     isRecentlyWatchedMoviesFetched: false,
+
     fetchRecentlyWatchedMovies: async () => {
       try {
-        const res = await axios.get("https://api.themoviedb.org/3/movie/1396", {
-          headers: {
-            accept: "application/json",
-            Authorization:
-              "Bearer " + process.env.NEXT_PUBLIC_TMDB_ACCESS_TOKEN,
-          },
-        });
-        set((state) => ({
-          recentlyWatchedMovies: [...state.recentlyWatchedMovies, res.data],
+        // * sample ids
+        const movieIds = [550, 299536, 603, 424];
+
+        const movieRequests = movieIds.map((id) =>
+          axios.get(`https://api.themoviedb.org/3/movie/${id}`, {
+            headers: {
+              accept: "application/json",
+              Authorization:
+                "Bearer " + process.env.NEXT_PUBLIC_TMDB_ACCESS_TOKEN,
+            },
+          })
+        );
+
+        const responses = await Promise.all(movieRequests);
+
+        const movieData = responses.map((res) => res.data);
+
+        set({
+          recentlyWatchedMovies: movieData,
           isRecentlyWatchedMoviesFetched: true,
-        }));
+        });
       } catch (error) {
         console.error("Error fetching recently watched movies:", error);
       }
--- Stores/Home/useRecentlyWatchedTvshowsStore.ts
+++ Stores/Home/useRecentlyWatchedTvshowsStore.ts
@@ -12,19 +12,30 @@ export const useRecentlyWatchedTvshowsStore =
   createStore<RecentlyWatchedTvshowsStore>()((set) => ({
     recentlyWatchedTvshows: [],
     isRecentlyWatchedTvshowsFetched: false,
+
     fetchRecentlyWatchedTvshows: async () => {
       try {
-        const res = await axios.get("https://api.themoviedb.org/3/tv/248852", {
-          headers: {
-            accept: "application/json",
-            Authorization:
-              "Bearer " + process.env.NEXT_PUBLIC_TMDB_ACCESS_TOKEN,
-          },
-        });
-        set((state) => ({
-          recentlyWatchedTvshows: [...state.recentlyWatchedTvshows, res.data],
+        // * sample ids
+        const tvShowIds = [1396, 66732, 2316, 1668];
+
+        const tvShowRequests = tvShowIds.map((id) =>
+          axios.get(`https://api.themoviedb.org/3/tv/${id}`, {
+            headers: {
+              accept: "application/json",
+              Authorization:
+                "Bearer " + process.env.NEXT_PUBLIC_TMDB_ACCESS_TOKEN,
+            },
+          })
+        );
+
+        const responses = await Promise.all(tvShowRequests);
+
+        const tvShowData = responses.map((res) => res.data);
+
+        set({
+          recentlyWatchedTvshows: tvShowData,
           isRecentlyWatchedTvshowsFetched: true,
-        }));
+        });
       } catch (error) {
         console.error("Error fetching recently watched tv shows:", error);
       }
