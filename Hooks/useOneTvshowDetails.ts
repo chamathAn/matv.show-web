0fe9402c35c6957df7b6b89c1867468ee38d4716
chamathan <chamathanjula2@gmail.com>
2025-09-01 00:14:53 +0530

    feat: tv shwows episodes ratings

--- Hooks/useOneTvshowDetails.ts
+++ Hooks/useOneTvshowDetails.ts
@@ -5,6 +5,7 @@ import { useStore } from "zustand";
 
 export default function useOneTvshowDetails() {
   const tvState = useStore(useOneTvshowDetailsStore);
+
   useEffect(() => {
     if (!tvState.isOneTvshowDetialsFetched) {
       useOneTvshowDetailsStore.getState().fetchOneTvshowDetails();
--- components/tv-shows/content/tv-seasons-episodes.tsx
+++ components/tv-shows/content/tv-seasons-episodes.tsx
@@ -1,14 +1,26 @@
 "use client";
 import useOneTvshowDetails from "@/Hooks/useOneTvshowDetails";
-import React from "react";
+import React, { useState } from "react";
 import {
   Accordion,
   AccordionContent,
   AccordionItem,
   AccordionTrigger,
 } from "@/components/ui/accordion";
+import { Rating } from "@fluentui/react-rating";
+import { Button } from "@/components/ui/button";
 export default function TvSeasonsEpisodes() {
   const { allSeasons } = useOneTvshowDetails();
+  const [userRating, setUserRating] = useState<{ [key: string]: number }>({});
+
+  // handle rating
+  const handleRatingChange = (episodeName: string, value: number) => {
+    setUserRating((prevRatings) => ({
+      ...prevRatings,
+      [episodeName]: value,
+    }));
+  };
+
   return (
     <section className="relative mt-10 px-6 sm:px-0 font-poppins w-full flex flex-col gap-y-5 z-20 overflow-hidden text-foreground">
       {/* accordion */}
@@ -16,7 +28,7 @@ export default function TvSeasonsEpisodes() {
         allSeasons.map((season, i) => (
           <Accordion key={i} type="single" collapsible>
             <AccordionItem value={season.name}>
-              <AccordionTrigger className="text-lg bg-gradient-to-r from-accent to-transparent">
+              <AccordionTrigger className="text-lg pl-2 sm:pl-4 bg-gradient-to-r from-accent to-transparent">
                 {season.name}
               </AccordionTrigger>
               <AccordionContent>
@@ -27,6 +39,17 @@ export default function TvSeasonsEpisodes() {
                       <AccordionTrigger>{episode.name}</AccordionTrigger>
                       <AccordionContent>
                         <p>{episode.overview}</p>
+
+                        {/* give your rating */}
+                        <div className="flex gap-5 items-center mt-5">
+                          <Rating
+                            value={userRating[episode.name] || 0}
+                            onChange={(_, data) =>
+                              handleRatingChange(episode.name, data.value)
+                            }
+                          />
+                          <Button variant="secondary">Rate</Button>
+                        </div>
                       </AccordionContent>
                     </AccordionItem>
                   ))}
